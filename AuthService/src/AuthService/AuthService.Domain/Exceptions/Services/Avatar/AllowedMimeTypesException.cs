826270e8b887f03ab9d907ab4e5352c58e34ce72
danill20002018-netizen <danill20002018@gmail.com>
2026-08-17 15:34:21 +0300

    add(AvatarFeat part): custom Exeption added

---
+++ AuthService/src/AuthService/AuthService.Domain/Exceptions/Services/Avatar/AllowedMimeTypesException.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthService.Domain.Exceptions.Services.Avatar;
+
+public class AllowedMimeTypesException: Exception
+{
+    string? FileContentType { get; set; }
+
+    public AllowedMimeTypesException(string? fileContentType) : base($"A file content type prohibited by policy was used: {(fileContentType==null ? $"\"{fileContentType}\"" : "content type missing")}")
+    {
+        FileContentType = fileContentType;
+    }
+}
